ce222c5eba2e22f2bc522b89c2a8969c4bcc5865
typeofNaN <dmdefine6@gmail.com>
2024-01-24 10:10:59 +0800

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -50,37 +50,24 @@ export default defineConfig(configEnv => {
       sourcemap: false,
       commonjsOptions: {
         ignoreTryCatch: false
-      }
-    },
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      input: {
-        index: 'index.html'
       },
-      // 静态资源分类打包
-      output: {
-        chunkFileNames: 'static/js/[name]-[hash].js',
-        entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-        // TODO: 处理GitHub Pages 部署 _plugin-vue_export-helper.js 404
-        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-        sanitizeFileName(name: string) {
-          const match = DRIVE_LETTER_REGEX.exec(name)
-          const driveLetter = match ? match[0] : ''
-          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
-          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
-          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
-        },
-        manualChunks(id: string) {
-          if (id.includes('node_modules')) {
-            return id.toString().match(/\/node_modules\/(?!.pnpm)(?<moduleName>[^\/]*)\//)?.groups!.moduleName ?? 'vender'
+      outDir: 'dist',
+      assetsDir: 'assets',
+      chunkSizeWarningLimit: 2000, // 解决包大小超过500kb的正告
+      rollupOptions: {
+        output: {
+          manualChunks: {},
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash].[ext]',
+          sanitizeFileName: (name) => {
+            const match = DRIVE_LETTER_REGEX.exec(name)
+            const driveLetter = match ? match[0] : ''
+            return (
+              driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '') // 解决文件名中的非法字符
+            )
           }
         }
-        // manualChunks(id) {
-        //   if (id.includes('node_modules')) {
-        //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
-        //   }
-        // }
       }
     }
   }
