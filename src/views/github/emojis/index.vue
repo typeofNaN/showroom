e9d981873692e21ed768a87f2ca4e5b7b944bf9e
typeofNaN <dmdefine6@gmail.com>
2024-03-15 14:56:49 +0800

    perf: useLoading hook

--- src/views/github/emojis/index.vue
+++ src/views/github/emojis/index.vue
@@ -8,29 +8,34 @@
         </template>
       </n-input>
     </n-card>
-    <n-row v-if="emojisList.length > 0" :gutter="[10, 10]">
-      <n-col v-for="i in emojisList" :span="4" :key="i.name">
-        <n-card :bordered="false" hoverable class="rounded-8px shadow-sm">
-          <div class="flex items-center h-40px cursor-copy" @click="copy(i.name)">
-            <n-image :src="i.url" lazy width="40" class="mr-10px" />
-            <span>{{ i.name }}</span>
-          </div>
-        </n-card>
-      </n-col>
-    </n-row>
-    <n-card v-else :bordered="false" class="rounded-8px shadow-sm">
-      <div class="flex justify-center items-center h-500px">
-        <n-empty v-if="isLoadingDone" :description="$t('common.noData')" size="huge" />
-        <n-spin v-else size="large" />
-      </div>
-    </n-card>
+    <div v-if="loading" class="flex justify-center items-center h-500px">
+      <n-spin size="large" />
+    </div>
+    <template v-else>
+      <n-row v-if="emojisList.length > 0" :gutter="[10, 10]">
+        <n-col v-for="i in emojisList" :span="4" :key="i.name">
+          <n-card :bordered="false" hoverable class="rounded-8px shadow-sm">
+            <div class="flex items-center h-40px cursor-copy" @click="copy(i.name)">
+              <n-image :src="i.url" lazy width="40" class="mr-10px" />
+              <span>{{ i.name }}</span>
+            </div>
+          </n-card>
+        </n-col>
+      </n-row>
+      <n-card v-else :bordered="false" class="rounded-8px shadow-sm">
+        <div class="flex justify-center items-center h-500px">
+          <n-empty :description="$t('common.noData')" size="huge" />
+        </div>
+      </n-card>
+    </template>
   </n-space>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
+import { cloneDeep } from 'lodash-es'
 
-import { useClipboard } from '@/hooks'
+import { useClipboard, useLoading } from '@/hooks'
 import { $t } from '@/locales'
 import { GithubApi } from '@/service'
 
@@ -39,46 +44,41 @@ type Emojis = {
   url: string
 }
 
+const { loading, startLoading, endLoading } = useLoading(true)
 const { copyText } = useClipboard()
 
 const inputValue = ref<string>('')
 
 let originEmojisList: Emojis[] = []
 const emojisList = ref<Emojis[]>([])
-const isLoadingDone = ref<boolean>(false)
 
 onMounted(async () => {
   await getGithubEmojisList()
 })
 
 async function getGithubEmojisList() {
-  try {
-    const { data } = await GithubApi.getEmojisList()
+  startLoading()
+  const { data } = await GithubApi.getEmojisList()
 
-    if (data) {
-      const list: Emojis[] = []
-      for (let i in data) {
-        list.push({
-          name: i,
-          url: data[i]
-        })
-      }
-
-      emojisList.value = list
-      originEmojisList = [...list]
-      isLoadingDone.value = true
+  if (data) {
+    const list: Emojis[] = []
+    for (let i in data) {
+      list.push({
+        name: i,
+        url: data[i]
+      })
     }
-  } catch {
-    isLoadingDone.value = true
+
+    emojisList.value = list
+    originEmojisList = cloneDeep(list)
   }
+  endLoading()
 }
 
 function handleInputChange(val: string) {
-  if (val) {
-    emojisList.value = originEmojisList.filter(emojis => emojis.name.includes(val.trim()))
-  } else {
-    emojisList.value = [...originEmojisList]
-  }
+  emojisList.value = val
+    ? originEmojisList.filter(emojis => emojis.name.includes(val.trim()))
+    : cloneDeep(originEmojisList)
 }
 
 function copy(text: string) {
