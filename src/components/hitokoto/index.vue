85ad43ab292edefda444d45e11b284754b89f537
typeofNaN <dmdefine6@gmail.com>
2024-03-19 17:46:38 +0800

    perf: hitokoto

--- src/components/hitokoto/index.vue
+++ src/components/hitokoto/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <n-card :bordered="false" class="rounded-8px shadow-sm" :title="$t('page.dashboard.hitokoto')">
-    <template #header-extra>
+  <n-card :bordered="false" class="rounded-8px shadow-sm">
+    <template #header>
+      <div class="flex items-center">
+        <svg-icon local-icon="topic" />
+        <span class="ml-10px text-16px">{{ $t('page.dashboard.hitokoto') }}</span>
+      </div>
+    </template>
+    <!-- <template #header-extra>
       <hover-container class="w-64px h-full" placement="bottom-end" @click="getHitokotoData">
         <icon-mdi-refresh class="text-22px" :class="{ 'animate-spin': loading }" />
       </hover-container>
-    </template>
+    </template> -->
     <template v-if="hitokotoData">
       <div class="text-center">
-        <div class="h-120px font-700 font-italic text-18px">“{{ hitokotoData.hitokoto }}”</div>
+        <div class="mb-40px font-700 font-italic text-18px">“{{ hitokotoData.hitokoto }}”</div>
         <div class="mb-10px font-700 text-16px ">《{{ hitokotoData.from }}》</div>
         <div class="flex justify-center items-center">
           <div class="mr-10px h-1px w-30px bg-#ccc" />
@@ -22,39 +28,39 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
 
-import { useLoading } from '@/hooks'
+// import { useLoading } from '@/hooks'
 import { $t } from '@/locales'
 import { HitokotoApi } from '@/service'
 
 const hitokotoData = ref<ApiHitokoto.IGetHitokotoDataRes>({
   commit_from: 'api',
-  created_at: '1541996434',
-  creator: '我眼中有星河万里',
-  creator_uid: 2311,
-  from: '九思',
-  from_who: '',
-  hitokoto: '故事的结局从来不人意！',
-  id: 4035,
-  length: 11,
-  reviewer: 0,
-  type: 'e',
-  uuid: 'b69df0ef-287e-4ae8-b728-81d50cc03dd4'
+  created_at: '1672677945',
+  creator: 'Jerry',
+  creator_uid: 13499,
+  from: '明日战记',
+  from_who: '郑重生',
+  hitokoto: '结局是什么，我们自己决定！',
+  id: 9142,
+  length: 13,
+  reviewer: 4756,
+  type: 'h',
+  uuid: 'd362384f-8db7-4c2e-9bb7-ffe906395a63'
 })
 
-const { loading, startLoading, endLoading } = useLoading()
+// const { loading, startLoading, endLoading } = useLoading()
 
 onMounted(async () => {
   await getHitokotoData()
 })
 
 async function getHitokotoData() {
-  startLoading()
+  // startLoading()
 
   const { data } = await HitokotoApi.getHitokotoData()
   if (data) {
     hitokotoData.value = data
   }
 
-  endLoading()
+  // endLoading()
 }
 </script>
