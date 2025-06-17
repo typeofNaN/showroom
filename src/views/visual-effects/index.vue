580e7f977ae9fb341c6f484e92cfb9f8194b3d05
typeofNaN <dmdefine6@gmail.com>
2025-06-17 11:25:47 +0800

    feat: honeycomb

--- src/views/visual-effects/index.vue
+++ src/views/visual-effects/index.vue
@@ -2,16 +2,15 @@
   <n-space vertical :size="16">
     <n-row :gutter="[16, 16]">
       <n-col v-for="(item, index) in list" :span="12" :key="index">
-        <n-card :title="$t(item.name)" :bordered="false" class="h-500px">
+        <div class="flex-center b-rd-4px bg-#fff h-500px">
           <component :is="item.component" />
-        </n-card>
+        </div>
       </n-col>
     </n-row>
   </n-space>
 </template>
 
 <script lang="ts" setup>
-import { $t } from '@/locales'
 import Honeycomb from './components/honeycomb/index.vue'
 
 type ListType = {
