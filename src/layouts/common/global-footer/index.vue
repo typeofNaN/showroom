68a607b8db7ef47b78e2a45db595744ad8f45c00
typeofNaN <dmdefine6@gmail.com>
2025-06-26 11:48:53 +0800

    feat: add author link in footer abr 🚀

--- src/layouts/common/global-footer/index.vue
+++ src/layouts/common/global-footer/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <dark-mode-container class="flex-center h-full" :inverted="theme.footer.inverted">
-    <span>©2024</span>
+  <dark-mode-container class="flex-center gap-10px h-full" :inverted="theme.footer.inverted">
+    <a :href="packageJson.author.url" target="_blank" class="text-primary cursor-pointer">
+      ©{{ packageJson.author.name }}
+    </a>
+    <span>- 2024</span>
   </dark-mode-container>
 </template>
 
 <script lang="ts" setup>
 import { useThemeStore } from '@/store'
+import packageJson from '~/package.json'
 
 defineOptions({ name: 'GlobalFooter' })
 
